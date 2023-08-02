fd6e08d8820df4829c167af2d5da56f4a616511c
Kasper Timm Hansen <hey@kaspth.com>
2023-08-02 18:32:53 +0200

    Use Teams::Base load hook to insert functionality (#10)
    * Use Teams::Base load hook to insert functionality
    
    This work is dependent on bullet-train-co/bullet_train-core#335 being
    shipped.
    
    Then we're just grabbing the bullet_train-billing-stripe specific extensions and pulling them in to our
    repo. Note: we can skip the surrounding `defined?(Billing::Stripe::Subscription)` checks, since we already know its enabled
    by the fact that we're loading the very library.
    
    * Use AbilitySupport load hook to insert functionality
    
    * Hook into BulletTrain::Billing.provider_subscription_attributes
    
    * Hide behind compatibility hook for backwardscompatibility

--- lib/bullet_train/billing/stripe.rb
+++ lib/bullet_train/billing/stripe.rb
@@ -4,7 +4,23 @@ require "bullet_train/billing/stripe/engine"
 module BulletTrain
   module Billing
     module Stripe
-      # Your code goes here...
+      module Teams
+        module Base
+          extend ActiveSupport::Concern
+
+          included do
+            has_many :billing_stripe_subscriptions, class_name: "Billing::Stripe::Subscription", dependent: :destroy, foreign_key: :team_id
+          end
+        end
+      end
+
+      module AbilitySupport
+        def apply_billing_abilities(user)
+          super
+          can :read, Billing::Stripe::Subscription, team_id: user.team_ids
+          can :manage, Billing::Stripe::Subscription, team_id: user.administrating_team_ids
+        end
+      end
     end
   end
 end
@@ -12,3 +28,6 @@ end
 def stripe_billing_enabled?
   ENV["STRIPE_SECRET_KEY"].present?
 end
+
+ActiveSupport.on_load(:bullet_train_teams_base) { include BulletTrain::Billing::Stripe::Teams::Base }
+ActiveSupport.on_load(:bullet_train_billing_ability_support) { prepend BulletTrain::Billing::Stripe::AbilitySupport }
