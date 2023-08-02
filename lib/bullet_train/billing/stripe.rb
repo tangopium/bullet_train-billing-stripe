422691852393d61d257de266557c23c60e786c3e
Kasper Timm Hansen <kaspth@gmail.com>
2023-08-02 18:39:12 +0200

    Revert "Use Teams::Base load hook to insert functionality (#10)"
    This reverts commit fd6e08d8820df4829c167af2d5da56f4a616511c.

--- lib/bullet_train/billing/stripe.rb
+++ lib/bullet_train/billing/stripe.rb
@@ -4,23 +4,7 @@ require "bullet_train/billing/stripe/engine"
 module BulletTrain
   module Billing
     module Stripe
-      module Teams
-        module Base
-          extend ActiveSupport::Concern
-
-          included do
-            has_many :billing_stripe_subscriptions, class_name: "Billing::Stripe::Subscription", dependent: :destroy, foreign_key: :team_id
-          end
-        end
-      end
-
-      module AbilitySupport
-        def apply_billing_abilities(user)
-          super
-          can :read, Billing::Stripe::Subscription, team_id: user.team_ids
-          can :manage, Billing::Stripe::Subscription, team_id: user.administrating_team_ids
-        end
-      end
+      # Your code goes here...
     end
   end
 end
@@ -28,6 +12,3 @@ end
 def stripe_billing_enabled?
   ENV["STRIPE_SECRET_KEY"].present?
 end
-
-ActiveSupport.on_load(:bullet_train_teams_base) { include BulletTrain::Billing::Stripe::Teams::Base }
-ActiveSupport.on_load(:bullet_train_billing_ability_support) { prepend BulletTrain::Billing::Stripe::AbilitySupport }
