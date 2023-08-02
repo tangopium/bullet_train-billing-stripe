c2610c624f376e34d0ab11d3bc46b2a0dd604c8c
Kasper Timm Hansen <hey@kaspth.com>
2023-08-02 19:02:54 +0200

    Revert "Revert "Use Teams::Base load hook to insert functionality (#1… (#15)
    * Revert "Revert "Use Teams::Base load hook to insert functionality (#10)""
    
    This reverts commit 422691852393d61d257de266557c23c60e786c3e.
    
    * Fully qualify constants and make sure we extend as concern

--- lib/bullet_train/billing/stripe/engine.rb
+++ lib/bullet_train/billing/stripe/engine.rb
@@ -2,6 +2,13 @@ module BulletTrain
   module Billing
     module Stripe
       class Engine < ::Rails::Engine
+        initializer "bullet_train-billing.integrate" do
+          config.after_initialize do
+            if defined?(BulletTrain::Billing.provider_subscription_attributes)
+              BulletTrain::Billing.provider_subscription_attributes << :stripe_subscription_id
+            end
+          end
+        end
       end
     end
   end
