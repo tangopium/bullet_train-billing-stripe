36aad5fb3e20353b48f46b58a3d271dbb376eb82
Jeremy Green <jeremy@octolabs.com>
2023-08-18 14:02:19 -0500

    Don't auto-merge FactoryBoth paths

--- lib/bullet_train/billing/stripe/engine.rb
+++ lib/bullet_train/billing/stripe/engine.rb
@@ -1,16 +1,7 @@
-begin
-  require "factory_bot_rails"
-rescue LoadError
-end
-
 module BulletTrain
   module Billing
     module Stripe
       class Engine < ::Rails::Engine
-        if defined? FactoryBotRails
-          config.factory_bot.definition_file_paths += [File.expand_path("../../../../factories", __FILE__)]
-        end
-
         initializer "bullet_train-billing.integrate" do
           config.after_initialize do
             if defined?(BulletTrain::Billing.provider_subscription_attributes)
